a29e9c3a66a1c98540b74f53e3097cfcdc18bc57
Pratikshya Pandey <pratikshya.pp19@gmail.com>
2019-03-22 23:50:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,9 +20,6 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
-programmer_hash[:alan_kay]
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -38,6 +35,7 @@ programmer_hash[:alan_kay]
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
